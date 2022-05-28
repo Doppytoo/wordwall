19c9fbf9a924830a0311f0ddc7005c999f2320c4
Doppytoo <mr.asmirnow@gmail.com>
2022-05-28 22:24:16 +0300

    Initial Commit

---
+++ lib/pages/input_page.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb;
+
+import 'package:flutter_scatter/flutter_scatter.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class InputPage extends StatelessWidget {
+  InputPage({Key? key}) : super(key: key);
+
+  final wordInputController = TextEditingController();
+
+  Future<void> submitWord(BuildContext context) async {
+    if (wordInputController.text.isEmpty) {
+      return;
+    }
+    final ref = FirebaseDatabase.instance.ref('words');
+    final snapshot = await ref.get();
+    if (snapshot.exists) {
+      ref.update({
+        wordInputController.text.toLowerCase(): (((snapshot.value!
+                    as Map)[wordInputController.text.toLowerCase()] ??
+                0) as int) +
+            1
+      });
+    } else {
+      ref.set({wordInputController.text.toLowerCase(): 1});
+    }
+    wordInputController.clear();
+    Navigator.of(context).pushNamed('/view');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        child: SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+                width: MediaQuery.of(context).size.width / 3,
+                child: CupertinoTextField(
+                  controller: wordInputController,
+                )),
+            SizedBox(
+              height: 8,
+            ),
+            CupertinoButton.filled(
+                child: Text('Submit'),
+                onPressed: () {
+                  submitWord(context);
+                })
+          ],
+        ),
+      ),
+    ));
+  }
+}
